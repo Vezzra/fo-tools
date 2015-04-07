fdfa1bd1e2acd7a551ea4f0733a4d2fa67d1ea1c
Vezzra <vezzra@gmx.net>
2015-04-07 15:09:36 +0200

    Added make_bo_archive.py script
    This script creates a zip archive containing only the FO binaries produced by a normal build on Windows.

---
+++ make_bo_archive.py
@@ -0,0 +1,36 @@
+import sys
+import os
+from datetime import datetime
+from subprocess import check_output
+from zipfile import ZipFile, ZIP_DEFLATED
+
+
+os.chdir(os.path.join(os.path.dirname(sys.argv[0]), "FreeOrion"))
+
+build_no = "XXXX"
+try:
+    commit = check_output(["git", "show", "-s", "--format=%h", "HEAD"]).strip()
+    timestamp = float(check_output(["git", "show", "-s", "--format=%ct", "HEAD"]).strip())
+    build_no = ".".join([datetime.utcfromtimestamp(timestamp).strftime("%Y%m%d"), commit])
+except:
+    print "WARNING: git not installed, can't determine build number"
+
+out_dir = os.path.dirname(sys.argv[0])
+out_file = "FreeOrion-%s-Test-Win32-BinariesOnly.zip" % build_no
+out_path = os.path.join(out_dir, out_file)
+if os.path.exists(out_file):
+    os.remove(out_file)
+
+in_files = [
+    "FreeOrion.exe",
+    "FreeOrionCA.exe",
+    "FreeOrionD.exe",
+    "GiGiSDL.dll",
+    "GiGi.dll",
+    "log4cpp.dll"
+]
+
+print "Creating %s in %s" % (out_file, out_dir)
+
+with ZipFile(out_path, "w", ZIP_DEFLATED) as ar:
+    map(ar.write, in_files)
