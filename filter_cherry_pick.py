7a79e263932728b7bdac284ac2cc713a07e40c6e
Vezzra <vezzra@gmx.net>
2015-08-04 14:04:40 +0200

    Python script for filtering cherry-picked commits

---
+++ filter_cherry_pick.py
@@ -0,0 +1,13 @@
+import sys
+from subprocess import check_output
+
+
+if len(sys.argv) != 3:
+    print "Usage: filter_cherry_pick.py <branch-to-cherry-pick-from> <branch-to-cherry-pick-into>"
+    quit()
+
+for sha in check_output(["git", "rev-list", "--cherry-pick", "--left-only", "--no-merges",
+                         "%s...%s" % (sys.argv[1], sys.argv[2])]).splitlines():
+    author = check_output(["git", "show", "-s", r"--format=%an", sha]).strip(" \n")
+    msg = check_output(["git", "show", "-s", r"--format=%s", sha]).strip(" \n")
+    print "%s   %s [%s]" % (sha[:7], msg[:50], author)
