4930a09d8a97c591d73435c1ab39d395061f172c
Vezzra <vezzra@gmx.net>
2016-12-15 15:51:40 +0100

    Revert "Fixed computation of build number"
    This reverts commit d64fc5f7f5c757280fba8a8f4cfdf0ec4a02d98f.

--- make_bo_archive.py
+++ make_bo_archive.py
@@ -9,7 +9,7 @@ os.chdir(os.path.join(os.path.dirname(sys.argv[0]), "FreeOrion"))
 
 build_no = "XXXX"
 try:
-    commit = check_output(["git", "show", "-s", "--format=%H", "HEAD"]).strip()[:7]
+    commit = check_output(["git", "show", "-s", "--format=%h", "HEAD"]).strip()
     timestamp = float(check_output(["git", "show", "-s", "--format=%ct", "HEAD"]).strip())
     build_no = ".".join([datetime.utcfromtimestamp(timestamp).strftime("%Y-%m-%d"), commit])
 except:
