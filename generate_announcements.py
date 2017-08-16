15343b7146fc79407339d2755ad42dace2b03f80
Vezzra <vezzra@gmx.net>
2017-08-16 13:11:49 +0200

    Add generate_announcements.py Python script

---
+++ generate_announcements.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+
+import sys
+import os
+from string import Template
+from datetime import datetime
+from subprocess import check_output
+
+
+announcement_template = Template('''==========
+New test build (build ${buildno}) available for Windows on sourceforge.
+
+[url=http://sourceforge.net/projects/freeorion/files/FreeOrion/Test/FreeOrion_${buildno}_Test_Win32_Setup.exe/download]Download link for full installer.[/url]
+
+[url=http://sourceforge.net/projects/freeorion/files/FreeOrion/Test/FreeOrion_${buildno}_Test_Win32_BinariesOnly.zip/download]Download link for zip archive with binaries only.[/url]
+==========
+New test build (build ${buildno}) available for Mac OSX 10.9 and later on sourceforge.
+
+[url=http://sourceforge.net/projects/freeorion/files/FreeOrion/Test/FreeOrion_${buildno}_MacOSX_10.9.dmg/download]Download link.[/url]
+==========
+New test builds available for Windows and OSX (build ${buildno}):
+https://sourceforge.net/projects/freeorion/files/FreeOrion/Test/
+==========
+''')
+
+
+build_no = "XXXX"
+try:
+    commit = check_output(["git", "show", "-s", "--format=%h", "--abbrev=7", "HEAD"]).strip()
+    timestamp = float(check_output(["git", "show", "-s", "--format=%ct", "HEAD"]).strip())
+    build_no = ".".join([datetime.utcfromtimestamp(timestamp).strftime("%Y-%m-%d"), commit])
+except:
+    print "WARNING: Can't determine build number"
+
+print announcement_template.substitute(buildno=build_no)
